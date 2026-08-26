c1b027fbe250cb4a0d7aeb1852ba983e9c221458
uniblab <uniblab@hotmail.com>
2026-08-26 03:02:43 -0400

    nullable

--- Icod.TermInfo.Source/src/TermInfoSourceCapabilityClassifier.cs
+++ Icod.TermInfo.Source/src/TermInfoSourceCapabilityClassifier.cs
@@ -40,21 +40,21 @@ internal static class TermInfoSourceCapabilityClassifier
                 name,
                 out StandardCapabilityMetadata<BooleanCapability>? boolean))
         {
-            return TermInfoSourceCapabilityIdentity.StandardBoolean(boolean);
+            return TermInfoSourceCapabilityIdentity.StandardBoolean(boolean!);
         }
 
         if (TryGetStandardNumeric(
                 name,
                 out StandardCapabilityMetadata<NumericCapability>? numeric))
         {
-            return TermInfoSourceCapabilityIdentity.StandardNumeric(numeric);
+            return TermInfoSourceCapabilityIdentity.StandardNumeric(numeric!);
         }
 
         if (TryGetStandardString(
                 name,
                 out StandardCapabilityMetadata<StringCapability>? text))
         {
-            return TermInfoSourceCapabilityIdentity.StandardString(text);
+            return TermInfoSourceCapabilityIdentity.StandardString(text!);
         }
 
         if (!IsValidExtendedName(name))
