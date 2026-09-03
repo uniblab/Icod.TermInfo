d23e68d0755aa8e47872e70b17bbbfaafa64327b
Timothy Bruce <uniblab@hotmail.com>
2026-09-03 02:59:11 -0400

    Advance Runtime coordinated version assertions to DA07

--- tests/Icod.TermInfo.Tests/src/T45CompletionGateTests.cs
+++ tests/Icod.TermInfo.Tests/src/T45CompletionGateTests.cs
@@ -30,7 +30,7 @@ public sealed class T45CompletionGateTests {
 					2 )[ 0 ];
 
 		Assert.Equal(
-			"1.10.0-Alpha-6",
+			"1.10.0-Alpha-7",
 			semanticVersion );
 	}
 
@@ -52,7 +52,7 @@ public sealed class T45CompletionGateTests {
 				LoadOptions.None );
 
 		Assert.Equal(
-			"1.10.0-Alpha-6",
+			"1.10.0-Alpha-7",
 			ReadRequiredProperty(
 				buildProperties,
 				"IcodTermInfoSuiteVersion" ) );
