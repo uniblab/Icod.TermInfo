c6bfc15c3f988a50407f9b291558827c17032b17
Timothy Bruce <uniblab@hotmail.com>
2026-09-12 11:52:41 -0400

    Normalize terminal environment test collection

--- tests/Icod.TermInfo.Tests/src/Environment/TerminalEnvironmentCollection.cs
+++ tests/Icod.TermInfo.Tests/src/Environment/TerminalEnvironmentCollection.cs
@@ -3,9 +3,9 @@ using Xunit;
 namespace Icod.TermInfo.Tests;
 
 [CollectionDefinition(
-    "TerminalEnvironment",
-    DisableParallelization = true)]
-public sealed class TerminalEnvironmentCollection
-{
-    public const string Name = "TerminalEnvironment";
+	"TerminalEnvironment",
+	DisableParallelization = true
+)]
+public sealed class TerminalEnvironmentCollection {
+	public const string Name = "TerminalEnvironment";
 }
