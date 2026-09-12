b10989745d51171d01a071b05000d900f8471746
Timothy Bruce <uniblab@hotmail.com>
2026-09-12 09:54:40 -0400

    Normalize VT102 profile style

--- src/Profiles/Vt102TerminalProfile.cs
+++ src/Profiles/Vt102TerminalProfile.cs
@@ -1,13 +1,11 @@
 namespace Icod.TermInfo;
 
-internal static class Vt102TerminalProfile
-{
-    internal static TerminalDescription Create()
-    {
-        return new TerminalDescriptionBuilder("vt102")
-            .SetDescription("DEC VT102")
-            .ApplyVt100Core()
-            .ApplyVt102Editing()
-            .Build();
-    }
+internal static class Vt102TerminalProfile {
+	internal static TerminalDescription Create() {
+		return new TerminalDescriptionBuilder( "vt102" )
+			.SetDescription( "DEC VT102" )
+			.ApplyVt100Core()
+			.ApplyVt102Editing()
+			.Build();
+	}
 }
