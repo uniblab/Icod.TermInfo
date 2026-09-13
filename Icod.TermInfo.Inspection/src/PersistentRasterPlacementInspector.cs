d179f7843a47f3d9607743036d5f62e68ea31195
Timothy Bruce <uniblab@hotmail.com>
2026-09-13 00:06:08 -0400

    fix: conform PG05 ternary to repository layout

--- Icod.TermInfo.Inspection/src/PersistentRasterPlacementInspector.cs
+++ Icod.TermInfo.Inspection/src/PersistentRasterPlacementInspector.cs
@@ -131,15 +131,13 @@ public static class PersistentRasterPlacementInspector {
 				);
 			}
 
-			effectiveProfile = (
-				callerEvidence is null
-					? occurrenceProfiles[ 0 ]
-					: Inspect(
-						lookup.Winner.Entry.Terminal,
-						callerEvidence,
-						options
-					)
-			)
+			effectiveProfile = ( callerEvidence is null )
+				? occurrenceProfiles[ 0 ]
+				: Inspect(
+					lookup.Winner.Entry.Terminal,
+					callerEvidence,
+					options
+				)
 			;
 		}
 
