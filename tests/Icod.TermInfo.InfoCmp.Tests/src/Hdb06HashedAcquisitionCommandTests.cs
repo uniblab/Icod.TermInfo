1e5bd58c4cfd0dcea42eeca83888c2348e3ee18d
Timothy Bruce <uniblab@hotmail.com>
2026-09-16 11:44:08 -0400

    test: qualify HDB06 infocmp RED harness

--- tests/Icod.TermInfo.InfoCmp.Tests/src/Hdb06HashedAcquisitionCommandTests.cs
+++ tests/Icod.TermInfo.InfoCmp.Tests/src/Hdb06HashedAcquisitionCommandTests.cs
@@ -221,7 +221,7 @@ public sealed class Hdb06HashedAcquisitionCommandTests {
 		byte[] store,
 		Func<string, Task> assertion
 	) {
-		string path = Path.GetTempFileName();
+		string path = System.IO.Path.GetTempFileName();
 		try {
 			await File.WriteAllBytesAsync( path, store );
 			await assertion( path );
@@ -235,8 +235,8 @@ public sealed class Hdb06HashedAcquisitionCommandTests {
 		byte[] second,
 		Func<string, string, Task> assertion
 	) {
-		string firstPath = Path.GetTempFileName();
-		string secondPath = Path.GetTempFileName();
+		string firstPath = System.IO.Path.GetTempFileName();
+		string secondPath = System.IO.Path.GetTempFileName();
 		try {
 			await File.WriteAllBytesAsync( firstPath, first );
 			await File.WriteAllBytesAsync( secondPath, second );
